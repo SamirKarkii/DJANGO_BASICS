d8a61c03e67bdf0317c289d15ead40188f852f9d
Samir Karki <samirkarki9861975252@gmail.com>
2026-05-11 21:25:06 +0545

    Moving to classed based views

--- myproject/students/urls.py
+++ myproject/students/urls.py
@@ -1,18 +1,29 @@
 # from django.urls import path 
 # from . import views
-# urlpatterns = [
+# urlpatterns = 
 #     path('',views.home), 
 #     path('about/',views.about),
 #     path('students/',views.students)
 # ]
 
 
+# from django.urls import path
+# from .views import home, student_list,create_student,student_detail,update_student,delete_student,StudentListView
+
+# urlpatterns = [
+#     path('', home),
+#     path('students/', student_list),
+
+#     path('students/<int:id>/', student_detail), #so this means when a ceratain student/any int/ is found it takes to student_details
+#    path('create/<int:age>/<str:address>/<int:course_id>/', create_student),
+#    path('update/<int:id>/<str:new_address>/',update_student),
+#    path('delete/<int:id>/',delete_student),
+#    path('students/', StudentListView.as_view())
+   
+# ]
+
 from django.urls import path
-from .views import home, student_list, student_detail
-
+from .views import StudentListView
 urlpatterns = [
-    path('', home),
-    path('students/', student_list),
-
-    path('students/<int:id>/', student_detail), #so this means when a ceratain student/any int/ is found it takes to student_details 
+    path('students/', StudentListView.as_view()),
 ]
--- myproject/students/views.py
+++ myproject/students/views.py
@@ -1,46 +1,101 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from .models import Student
+# # from django.shortcuts import render
+# # from django.http import HttpResponse
+# # from .models import Student
 
-# def home(request):
-#     data = {
-#         'name':'samir',
-#         'age': '21'
-#     }
+# # def home(request):
+# #     data = {
+# #         'name':'samir',
+# #         'age': '21'
+# #     }
     
-#     return render(request,'home.html',data)
+# #     return render(request,'home.html',data)
 
-# def about(request):
-#     return HttpResponse("We’re passionate about building simple, powerful web applications with Django. Our goal is to make learning and development smooth, fun, and practical  for everyone. With clean design and clear code, we turn ideas into working projects effortlessly.")
+# # def about(request):
+# #     return HttpResponse("We’re passionate about building simple, powerful web applications with Django. Our goal is to make learning and development smooth, fun, and practical  for everyone. With clean design and clear code, we turn ideas into working projects effortlessly.")
 
-# def students(request):
-#     return HttpResponse("This view describes about student")
+# # def students(request):
+# #     return HttpResponse("This view describes about student")
 
-# def student(request):
-#     data = Student.objects.all()
-#     return render(request,'students.html', {'students':data})
+# # def student(request):
+# #     data = Student.objects.all()
+# #     return render(request,'students.html', {'students':data})
 
-# from django.http import HttpResponse 
-
-
+# # from django.http import HttpResponse 
 
 
+
+
+# from django.http import HttpResponse
+# from .models import Student,Course
+
+
+# # def home(request): 
+# #     return HttpResponse("Hello Django")
+
+# # def student_list(request):
+
+# #     students = Student.objects.all()
+# #     student_name = []
+# #     for student in students:
+# #         student_name.append(student.address)
+
+# #     return HttpResponse(",".join(student_name))
+
+# # def student_detail(request, id):
+# #     student = Student.objects.get(id=id) # id =1
+
+# #     return HttpResponse(student.address)
+
+# # #Read 
+# def student_list(request):
+#     students= Student.objects.all()
+#     data = []
+#     for s in students:
+#         data.append(f'{s.id}-{s.address}')
+#     return HttpResponse(",".join(data))
+
+# # #Create 
+# def create_student(request, age, address, course_id):
+#     course = Course.objects.get(id=course_id)
+
+#     Student.objects.create(
+#         age=age,
+#         address=address,
+#         course=course
+#     )
+
+#     return HttpResponse("Student created")
+
+# # #Update
+# def update_student(request,id,new_address):
+#     student  = Student.objects.get(id=id)
+#     student.address = new_address
+#     student.save() #this tells django to save the object in the databasee!!
+
+#     return HttpResponse("Student updated ")
+
+# # #Delete 
+# def delete_student(request,id):
+#     student = Student.objects.get(id=id)
+#     student.delete()
+#     return HttpResponse("Student deleted")
+
+
+
+from django.views import View
 from django.http import HttpResponse
-from .models import Student
+from .models import Student,Course
 
-def home(request): 
-    return HttpResponse("Hello Django")
 
-def student_list(request):
+#Simple Classed Based Views 
+class StudentListView(View):
+    def get(self,request):
+        students = Student.objects.all()
 
-    students = Student.objects.all()
-    student_name = []
-    for student in students:
-        student_name.append(student.address)
-
-    return HttpResponse(",".join(student_name))
-
-def student_detail(request, id):
-    student = Student.objects.get(id=id) # id =1
-
-    return HttpResponse(student.address)
+        data = []
+        for s in students: 
+            data.append(f'{s.id}-{s.address}')
+        return HttpResponse(",".join(data))
+    
+    def post(self,request):
+        return HttpResponse("post request")
